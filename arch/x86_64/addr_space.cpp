cb33a7d6f582c825bc1214c12e02a1cffc4f7eff
Alexander Ulmer <ulmer@student.tugraz.at>
2021-09-05 21:45:31 +0200

    fix AddrSpace::map() indentation

--- arch/x86_64/addr_space.cpp
+++ arch/x86_64/addr_space.cpp
@@ -85,66 +85,73 @@ static inline void tlb_invalidate(size_t virt)
 
 void AddrSpace::map(size_t virt, size_t phys, int flags)
 {
-    auto pageMap = PageMap::get();
+  auto pageMap = PageMap::get();
 
-    /* first, pre-compute all the indices into the different
-     * page tables. for example tableIndices[3] will be the
-     * index into the PML4 table, whereas tableIndex[0] will
-     * be the index into the page table. */
-    size_t tableIndices[4];
-    tableIndices[3] = (virt >> 27) & 0x1ff;     // PML4
-    tableIndices[2] = (virt >> 18) & 0x1ff;     // PDPT
-    tableIndices[1] = (virt >> 9) & 0x1ff;      // PDIR
-    tableIndices[0] = (virt) & 0x1ff;           // PTAB
+  /* first, pre-compute all the indices into the different
+   * page tables. for example tableIndices[3] will be the
+   * index into the PML4 table, whereas tableIndex[0] will
+   * be the index into the page table. */
+  size_t tableIndices[4];
+  tableIndices[3] = (virt >> 27) & 0x1ff;     // PML4
+  tableIndices[2] = (virt >> 18) & 0x1ff;     // PDPT
+  tableIndices[1] = (virt >> 9) & 0x1ff;      // PDIR
+  tableIndices[0] = (virt) & 0x1ff;           // PTAB
 
-    size_t currentTablePPN = m_pml4;
-    for (int level = 3; level >= 0; level--)
+  size_t currentTablePPN = m_pml4;
+  for (int level = 3; level >= 0; level--)
+  {
+    /* Compute a reference to the entry in the current table */
+    auto& currentLevelTableEntry =
+        ((GenericPagingTable*)PPN_TO_VIRT(currentTablePPN))[tableIndices[level]];
+
+    if (level == 0)
     {
-        /* Compute a reference to the entry in the current table */
-        auto& currentLevelTableEntry =
-            ((GenericPagingTable*)PPN_TO_VIRT(currentTablePPN))[tableIndices[level]];
-
-        /* Check if an entry is present for the current table,
-         * if not, we'll have to add one and allocate a new sub-table. */
-        if (!currentLevelTableEntry.present)
-        {
-            size_t newTablePPN = phys;
-            if (level != 0)
-            {
-                /* in case we're not yet at the page table level,
-                 * we need to allocate and clear a new page table
-                 * and clear it instead of just assigning the phys PPN. */
-                newTablePPN = pageMap.alloc();
-                memset(PPN_TO_VIRT(newTablePPN), 0, PAGE_SIZE);
-            }
-
-            /* clear the entry and fill with new data */
-            *((uint64_t*)&currentLevelTableEntry) = 0;
-            currentLevelTableEntry.present = 1;
-            currentLevelTableEntry.ppn = newTablePPN;
-
-            if (level == 0)
-            {
-                /* only set protections for actual pages */
-                if (flags & MAP_WRITE)  currentLevelTableEntry.write = 1;
-                if (flags & MAP_USER)   currentLevelTableEntry.user = 1;
-                if (flags & MAP_NOEXEC) currentLevelTableEntry.no_exec = 1;
-            }
-            else
-            {
-                /* be generous on the upper levels */
-                currentLevelTableEntry.write = 1;
-                currentLevelTableEntry.user = 1;
-            }
-        }
-
-        currentTablePPN = currentLevelTableEntry.ppn;
+      assert(!currentLevelTableEntry.present);
     }
+
+    /* Check if an entry is present for the current table,
+     * if not, we'll have to add one and allocate a new sub-table. */
+    if (!currentLevelTableEntry.present)
+    {
+      size_t newTablePPN = phys;
+      if (level != 0)
+      {
+        /* in case we're not yet at the page table level,
+         * we need to allocate and clear a new page table
+         * and clear it instead of just assigning the phys PPN. */
+        newTablePPN = pageMap.alloc();
+        memset(PPN_TO_VIRT(newTablePPN), 0, PAGE_SIZE);
+      }
+
+      /* clear the entry and fill with new data */
+      *((uint64_t*)&currentLevelTableEntry) = 0;
+      currentLevelTableEntry.present = 1;
+      currentLevelTableEntry.ppn = newTablePPN;
+
+      if (level == 0)
+      {
+        /* only set protections for actual pages */
+        if (flags & MAP_WRITE)  currentLevelTableEntry.write = 1;
+        if (flags & MAP_USER)   currentLevelTableEntry.user = 1;
+        if (flags & MAP_NOEXEC) currentLevelTableEntry.no_exec = 1;
+      }
+      else
+      {
+        /* be generous on the upper levels */
+        currentLevelTableEntry.write = 1;
+        currentLevelTableEntry.user = 1;
+      }
+    }
+
+    currentTablePPN = currentLevelTableEntry.ppn;
+  }
 }
 
 void AddrSpace::unmap(size_t virt)
 {
+
   assert(false);
+  tlb_invalidate(virt);
 }
 
 void AddrSpace::updateKernelMappings()
