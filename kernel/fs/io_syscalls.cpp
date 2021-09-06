5603e92310d5ca98ec482308e73475ab465577cf
Alexander Ulmer <ulmer@student.tugraz.at>
2021-09-06 14:51:30 +0200

    fix bug in syscall() assembly routine

--- kernel/fs/io_syscalls.cpp
+++ kernel/fs/io_syscalls.cpp
@@ -1,12 +1,13 @@
 #include <syscalls.h>
 #include <errno.h>
+#include <debug.h>
 
 ssize_t sys_read(int fd, char* buffer, size_t len)
 {
-  return -ENOTSUP;
+  return len;
 }
 
 ssize_t sys_write(int fd, char* buffer, size_t len)
 {
-  return -ENOTSUP;
+  return len;
 }
