b5fa324daffa5e5482743981e5b8ed5effce52da
Alexander Ulmer <ulmer@student.tugraz.at>
2021-09-11 15:21:39 +0200

    fix #9: unmap pages when address space is deleted

--- arch/x86_common/include/addr_space.h
+++ arch/x86_common/include/addr_space.h
@@ -24,7 +24,7 @@ class AddrSpace
     AddrSpace(const AddrSpace& self) = delete;
 
     AddrSpace* clone();
-    ~AddrSpace() = default;
+    ~AddrSpace();
 
     static void setup();
     static AddrSpace& kernel();
