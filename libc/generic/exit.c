66856829d38ad6a41d232fa29f085720af30323a
Alexander Ulmer <ulmer@student.tugraz.at>
2021-09-12 22:20:29 +0200

    implement exit()

--- libc/generic/exit.c
+++ libc/generic/exit.c
@@ -6,6 +6,11 @@ _Noreturn void _exit(int status)
 	_Exit(status);
 }
 
+_Noreturn void exit(int status)
+{
+  _Exit(status);
+}
+
 _Noreturn void __assert_fail(const char *expr, const char *file, int line, const char *func)
 {
 	fprintf(stderr, "Assertion failed: %s (%s: %s: %d)\n", expr, file, func, line);
