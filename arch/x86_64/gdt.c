3e72e3810875912c8bfa2f137974ed7b8e123697
Alexander Ulmer <ulmer@student.tugraz.at>
2021-09-13 00:20:59 +0200

    license headers in arch/

--- arch/x86_64/gdt.c
+++ arch/x86_64/gdt.c
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2017-2021 Alexander Ulmer <alexulmer1999@gmail.com>
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
 /*
  * UlmerOS x86_64 Global descriptor table and Task state segment
  * Copyright (C) 2017-2021 Alexander Ulmer
