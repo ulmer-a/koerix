f18d24d60296e59d18b492ba0c00db5b0fdbfcdf
Alexander Ulmer <ulmer@student.tugraz.at>
2021-09-19 20:17:28 +0200

    optimize atomic operations in assembly

--- kernel/include/sync/atomic.h
+++ kernel/include/sync/atomic.h
@@ -5,6 +5,8 @@
 
 #include <types.h>
 
-size_t xchg(size_t val, size_t* mem);
-uint8_t atomic_add8(uint8_t* mem, int8_t increment);
-size_t atomic_add(size_t* mem, ssize_t increment);
+extern "C" {
+  size_t xchg(size_t val, size_t* mem);
+  uint8_t atomic_add8(uint8_t* mem, int8_t increment);
+  size_t atomic_add(size_t* mem, ssize_t increment);
+}
