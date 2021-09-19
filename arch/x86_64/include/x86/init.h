3bb0976cbb684c937aa49fb79ab8d5f2b89b3129
Alexander Ulmer <ulmer@student.tugraz.at>
2021-09-19 21:45:39 +0200

    reformat start.cpp

--- arch/x86_64/include/x86/init.h
+++ arch/x86_64/include/x86/init.h
@@ -1,1 +1,15 @@
+// SPDX-FileCopyrightText: 2017-2021 Alexander Ulmer
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
 #pragma once
+
+/* kernel initialization function */
+extern void kernel_init(const char* cmdline);
+static void setup_task(void* cmdline) {
+  kernel_init((char*)cmdline);
+}
+
+extern "C" void x86_irq_init();
+extern void initHeap();
+extern void create_page_bitmap();
+extern void debug_init();
--- arch/x86_64/start.cpp
+++ arch/x86_64/start.cpp
@@ -4,20 +4,17 @@
 #include <types.h>
 #include <x86/init.h>
 #include <x86/stivale.h>
-
-
 #include <platform.h>
 #include <debug.h>
 #include <addr_space.h>
 #include <scheduler.h>
 #include <kernel_task.h>
-#include <lib/string.h>
 
+stivale_struct* s_stivale;
 
 // setup stack allocation
 static const size_t SETUP_STACK_SIZE = 1024*16;
 static uint8_t _INIT _ALIGNED(16) setup_stack[SETUP_STACK_SIZE];
-stivale_struct* s_stivale;
 
 _SECTION(".stivalehdr")
 static struct stivale_header stivale_hdr = {
@@ -27,11 +24,7 @@ static struct stivale_header stivale_hdr = {
   // BIT(0) will ask for a graphical framebuffer
   // BIT(3) all pointers received from the bootloader
   //   will be higher-half pointers.
- #ifdef i386
-  .flags = BIT(0),
- #else
   .flags = BIT(0) | BIT(3),
- #endif
 
   // let the bootloader decide the framebuffer size
   .framebuffer_width = 0,
@@ -41,19 +34,6 @@ static struct stivale_header stivale_hdr = {
   // entry point as in the corresponding ELF field
   .entry_point = 0
 };
-extern void kernel_init(const char* cmdline);
-static void setup_task(void* cmdline) {
-  kernel_init((char*)cmdline);
-}
-
-extern "C" void setup_gdt();
-extern "C" void x86_irq_init();
-extern void initHeap();
-extern void create_page_bitmap();
-
-bool s_sseEnabled = false;
-
-extern void debug_init();
 
 extern "C" void _NORETURN x86_init(struct stivale_struct *stivale)
 {
