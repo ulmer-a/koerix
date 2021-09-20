6c02349f3fe54d1839611c46c54a4ffda8a5a698
Alexander Ulmer <ulmer@student.tugraz.at>
2021-09-20 12:43:33 +0200

    ps/2: add device identification, proper handling

--- platform/pc/drivers/ps2.cpp
+++ platform/pc/drivers/ps2.cpp
@@ -20,6 +20,16 @@ enum PS2Registers
   STATUS_PORT = 0x64
 };
 
+enum PS2DeviceTypes
+{
+  PS2_KEYBOARD,
+  PS2_KEYBOARD_MF2,
+  PS2_MOUSE,
+  PS2_MOUSE_SCROLL,
+  PS2_MOUSE_5BUTTON,
+  OTHER
+};
+
 struct PS2ConfigByte
 {
   uint8_t irqPort1      : 1;
@@ -83,27 +93,85 @@ static bool ps2SelfTest()
   return result == 0x55 || result == 0x65;
 }
 
-static void ps2SetPort1IrqEnable(bool enable = true)
+static void ps2SetIrqEnable(int channel, bool enable = true)
 {
   auto config = ps2ReadConfig();
-  config.irqPort1 = enable;
+  if (channel == 0)
+    config.irqPort1 = enable;
+  else
+    config.irqPort2 = enable;
   ps2WriteConfig(config);
 }
 
-static void ps2SetPort1Enable(bool enable = true)
+static void ps2SetEnable(int channel, bool enable = true)
 {
-  outb(CMD_PORT, enable ? 0xae : 0xad);
+  if (channel == 0)
+    outb(CMD_PORT, enable ? 0xae : 0xad);
+  else
+    outb(CMD_PORT, enable ? 0xa8 : 0xa7);
 }
 
-static void ps2SetPort2Enable(bool enable = true)
+static bool ps2WaitForAck()
 {
-  outb(CMD_PORT, enable ? 0xa8 : 0xa7);
+  while (!ps2ReadStatus().outBufferFull)
+    sched::yield();
+  return inb(DATA_PORT) == 0xfa;
 }
 
-class PS2Keyboard : public devices::UartDevice
+static bool ps2WaitForByteTimeout(uint8_t& c)
+{
+  size_t attempts = 10;
+  while (attempts-- && !ps2ReadStatus().outBufferFull)
+    sched::yield();
+  if (attempts == 0)
+    return false;
+  c = inb(DATA_PORT);
+  return true;
+}
+
+static PS2DeviceTypes ps2Identify()
+{
+  /* identify the device */
+  outb(DATA_PORT, 0xf2);
+  ps2WaitForAck();
+
+  uint8_t responseBytes = 0;
+  uint8_t response[2];
+
+  size_t maxBytes = 2;
+  while (maxBytes--) {
+    uint8_t c;
+    if (!ps2WaitForByteTimeout(c))
+      break;
+    response[responseBytes++] = c;
+  }
+
+  if (responseBytes == 0)
+    return PS2_KEYBOARD;
+  if (responseBytes == 1)
+  {
+    if (response[0] == 0x00)
+      return PS2_MOUSE;
+    if (response[0] == 0x03)
+      return PS2_MOUSE_SCROLL;
+    if (response[0] == 0x04)
+      return PS2_MOUSE_5BUTTON;
+  }
+  else
+  {
+    if (response[0] == 0xab)
+    {
+      if (response[1] == 0x83)
+        return PS2_KEYBOARD_MF2;
+    }
+  }
+  return OTHER;
+}
+
+class PS2Device : public devices::UartDevice
 {
   public:
-    PS2Keyboard();
+    PS2Device(int channel);
 
     ssize_t write(char*, size_t) {
       return -ENOTSUP;
@@ -113,18 +181,38 @@ class PS2Keyboard : public devices::UartDevice
       char c = inb(DATA_PORT);
       receive(&c, 1);
     }
+
+  private:
+    PS2DeviceTypes m_type;
 };
 
-static void irqHandler(PS2Keyboard* kbd)
+static void irqHandler(PS2Device* dev)
 {
-  kbd->irq();
+  dev->irq();
 }
 
-PS2Keyboard::PS2Keyboard()
+PS2Device::PS2Device(int channel)
 {
-  ps2SetPort1Enable();
-  ps2SetPort1IrqEnable();
-  registerIrq(1, (void*)irqHandler, this);
+  /* enable the device, and temporarily disable
+   * the other device (todo) */
+  ps2SetEnable(channel, true);
+
+  /* identify the device type */
+  outb(DATA_PORT, 0xf5);
+  ps2WaitForAck();
+  m_type = ps2Identify();
+  outb(DATA_PORT, 0xf4);
+  ps2WaitForAck();
+
+  ps2SetIrqEnable(channel, true);
+  registerIrq(channel == 0 ? 1 : 12, (void*)irqHandler, this);
+
+  const char* device_name;
+  if (m_type == PS2_KEYBOARD || m_type == PS2_KEYBOARD_MF2)
+    device_name = "keyboard";
+  else
+    device_name = "mouse";
+  dev::registerDevice(device_name, this);
 }
 
 void ps2_init()
@@ -132,8 +220,8 @@ void ps2_init()
   assert(sizeof(PS2ConfigByte) == 1);
 
   /* disable both ports during init */
-  ps2SetPort1Enable(false);
-  ps2SetPort2Enable(false);
+  ps2SetEnable(0, false);
+  ps2SetEnable(1, false);
 
   ps2FlushOutBuffer();
 
@@ -158,16 +246,18 @@ void ps2_init()
   bool isDualChannel = false;
   if (couldBeDualChannel)
   {
-    ps2SetPort2Enable();
+    ps2SetEnable(1, true);
     config = ps2ReadConfig();
     if (config.clkPort2 == 0)
       isDualChannel = true;
-    ps2SetPort2Enable(false);
+    ps2SetEnable(1, false);
   }
   debug(DEVICES) << "ps/2 channels: "
                  << (isDualChannel ? "2" : "1") << "\n";
 
-
-  auto kbd = new PS2Keyboard();
-  dev::registerDevice("keyboard", kbd);
+  /* create device files */
+  new PS2Device(0);
+  // for now, disregard second channel devices
+  //if (isDualChannel)
+  //  new PS2Device(1);
 }
